6dea104a6e6645b292787e7ad5089d1c68d9b700
beauty-boys <2369976351@qq.com>
2025-02-17 15:18:39 +0800

    全局唯一ID生成策略：Redis自增 时间戳+订单 - UUID - Redis 自增 - snowflake 算法 - 数据库自增

--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,16 +1,50 @@
 package com.hmdp;
 
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
     @Resource
     private ShopServiceImpl shopService;
+
+    @Resource
+    private CacheClient cacheClient;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+
+    @Test
+    void testIdWorker() throws InterruptedException {
+        CountDownLatch letch = new CountDownLatch(300);
+
+        Runnable runnable = () -> {
+            for (int i = 0; i < 10; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            letch.countDown();
+        };
+        long start = System.currentTimeMillis();
+        for(int i=0;i<300;i++){
+            es.submit(runnable);
+        }
+        letch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time="+(end-start));
+
+    }
 
     @Test
     void testSaveShop() throws InterruptedException {
